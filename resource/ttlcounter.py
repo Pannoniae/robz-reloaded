8363b4f9c91eac0ab83f06fc24f4822ac1487f4e
Pannoniae <prisznyak.zsombor@gmail.com>
2022-10-15 13:48:11 +0100

    cleanup some retard files

--- resource/ttlcounter.py
+++ resource/ttlcounter.py
@@ -2,5 +2,5 @@ import os
 
 for root, dirs, files in os.walk("./"):
     for name in files:
-        if name.endswith("pdn"):
+        if name.endswith("psd"):
             os.remove(os.path.join(root, name))
