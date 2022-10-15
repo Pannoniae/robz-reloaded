b25afe13d86a73925e84e51f55597cd4cfe59b7a
Pannoniae <prisznyak.zsombor@gmail.com>
2022-10-15 19:15:39 +0100

    remove utterly unused helmets

---
+++ resource/set/breed/mp/usa/eqcounter.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+used_skins = set()
+
+for root, dirs, files in os.walk("../"):
+    for name in files:
+        with open(os.path.join(root, name)) as f:
+            for line in f:
+                try:
+                    hd = line.index("head")
+                    equipment = line[hd+5:].strip("}\"\n")
+                    used_skins.add(equipment)
+                except:
+                    continue
+print(used_skins)
+for root, dirs, files in os.walk("../../../../entity/inventory/-equipment/head/"):
+    for dir in dirs:
+        if dir not in used_skins and len(dir) > 6:
+            shutil.rmtree(os.path.join(root, dir))
